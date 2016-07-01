a6fcf7f7ac0229dcf094c514fad23f3a425fe82c
GPAMC <synyster_gc@hotmail.com>
2016-07-01 19:23:25 +0100

    Initial

--- Awesomeness.swift
+++ Awesomeness.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import Alamofire
+import AlamofireObjectMapper
+
+
+public class Awesomeness {
+    
+    
+    public class func thisIsAwesome(awesomeString: String) {
+        print("\(awesomeString) is Awesome!!!")
+    }
+    
+    public class func jsonResponse() {
+        
+        Alamofire.request(.GET, "http://blog.teamtreehouse.com/api/get_recent_summary/?count=20", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+    }
+}
